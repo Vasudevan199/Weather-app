c77014cb27f53faec1b9f9644033415f7944ef06
Vasudevan M <mvasudevan1992003@gmail.com>
2024-07-14 20:33:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const Weather=document.getquerySelector(".Weather");
+const cityInput=document.getquerySelector(".cityInput");
+const card=document.getquerySelector(".card");
+const apikey="9893a559b6a494347b27f5331c89b183";
+Weather.addEventListener("submit")
